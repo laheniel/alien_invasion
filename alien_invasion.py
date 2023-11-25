a7d53d2a2a02aad5be409e8342845fb079334f3b
laheniel <code.niellahe@proton.me>
2023-11-25 07:11:58 +0100

    Added a ship, screen updater and event listener

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,11 @@
 import sys
+from typing import Self
 
 import pygame
+
+from settings import Settings
+
+from ship import Ship
 
 
 class AlienInvasion:
@@ -10,27 +15,34 @@ class AlienInvasion:
         """Initialize the game, and create game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
-        # Set background color.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keyboard presses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
